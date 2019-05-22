5553e4d2c95ef9345f07a99d67ae329723dd055b
Douglas Matos <douglas.matos@adm.unip.br>
2019-05-22 15:20:18 -0300

    adicionado o projeto feito em aula

---
+++ wear/src/main/java/com/example/logonrmlocal/demowear/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.logonrmlocal.demowear
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import android.widget.Toast
+import com.example.shared.Carro
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.wearable.Wearable
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : WearableActivity(), GoogleApiClient.ConnectionCallbacks {
+
+
+    private lateinit var client: GoogleApiClient
+
+    override fun onConnected(p0: Bundle?) {
+        Wearable.MessageApi.addListener(client){
+            val carro = Gson().fromJson(String(it.data), Carro::class.java)
+
+            Toast.makeText(this, carro.modelo, Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Enables Always-on
+        setAmbientEnabled()
+
+        btClicaAqui.setOnClickListener {
+            Toast.makeText(this, "Fui clicado", Toast.LENGTH_LONG).show()
+        }
+
+        client = GoogleApiClient
+                .Builder(this)
+                .addConnectionCallbacks(this)
+                .addApi(Wearable.API)
+                .build()
+        client.connect()
+    }
+}
